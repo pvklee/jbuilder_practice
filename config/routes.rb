df18ac9463339515af0e861db34cbcaa16698691
Kevin Lee <pvklee@ucla.edu>
2019-05-06 09:00:04 -0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  # Your routes here!
+
+  namespace :api, defaults: { format: :json } do
+    # Your routes here
+    resources :gifts, only: [:show]
+    resources :parties, only: [:index, :show]
+    resources :guests, only: [:index, :show] do
+      resources :gifts, only: [:index]
+    end
+  end
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
